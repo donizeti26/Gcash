314438024fe72335e7349a847c868d05cccef388
Donizet Silva <61122214+donizeti26@users.noreply.github.com>
2025-05-31 11:04:25 -0300

    adicionando modal para criar despesa, criado js com fetch

---
+++ script/index.js
@@ -0,0 +1,32 @@
+document.querySelector(".buttonCreate").addEventListener("click", function () {
+  var opcoes = document.getElementById("opcoes")
+  var allButton = document.querySelector(".buttonCreate");
+
+  var button = document.querySelector(".add_up")
+  if (!opcoes.classList.contains("open")) {
+    opcoes.classList.add("open")
+    allButton.classList.add('remove_or_error')
+    button.classList.add('rotate_x')
+    allButton.classList.remove('buttonHover')
+  } else {
+    opcoes.classList.remove("open")
+    button.classList.remove('rotate_x')
+    allButton.classList.remove('remove_or_error')
+    allButton.classList.add('buttonHover')
+
+  }
+})
+
+
+
+function openModal(arquivo) {
+  fetch(arquivo)
+    .then(resposta => resposta.text())
+    .then(html => {
+      document.getElementById('modalContainer').innerHTML = html;
+    })
+}
+
+function fecharModal() {
+  document.getElementById('modalContainer').innerHTML = ''
+}
