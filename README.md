6bdd76d28c603e81f3178c13d8635e221a295749
donizeti26 <silva.donizeti26@gmail.com>
2025-08-29 22:30:05 -0300

    adicionando menu de icones na area de nova categoria

---
+++ README.md
@@ -0,0 +1,15 @@
+# 💰 Gcash - Gerenciador Financeiro Pessoal
+
+🎯 O Gcash foi criado para ajudar você a se organizar financeiramente, permitindo acompanhar todos os seus gastos e visualizar quanto você realmente gasta, mensalmente ou anualmente.
+
+# Funcionalidades
+
+Plataforma online para gerenciamento de lucros e despesas pessoais.
+
+Registro de gastos com categorias, valores, parcelas e status de pago ou não pago.
+
+Resumo mensal dos gastos, destacando total gasto e total pago.
+
+Facilita o planejamento financeiro e o controle do orçamento.
+
+Com o Gcash, manter suas finanças em dia nunca foi tão simples!
--- script/index.js
+++ script/index.js
@@ -23,6 +23,7 @@ function openModal(arquivo) {
     .then(html => {
       document.getElementById('modalContainer').innerHTML = html;
 
+
       // agora o HTML já está dentro do modalContainer
       const question_repet = document.getElementById("question_repet");
       var valueEvent
@@ -37,9 +38,12 @@ function openModal(arquivo) {
 
           });
         });
-
+        installments = document.getElementById("installments");
+        function createElements() {
+          var option = document.createElement("option")
+          installments.appendChild(option)
+        }
         function onofOption() {
-          installments = document.getElementById("installments");
           if (valueEvent === 0) {
             installments.disabled = true
           } else if (valueEvent === 1) {
@@ -73,6 +77,95 @@ modalContainer.addEventListener('click', function (e) {
   }
 });
 
+/*
+----------PAGINA INCIAL VOLTAR----------
+
+body = document.getElementsByTagName("body")
+
+body.addEventListener('click', function (e) {
+  const button_option = document.getElementById('button_option');
+  if (e.target === button_option) {
+    fecharModal();
+  }
+});
+
+
+*/
+/*-------------MENU ICONS--------------*/
+const bank_icons = [
+  { icon: "shopping_cart", text: "Compras" },
+  { icon: "restaurant", text: "Alimentação" },
+  { icon: "local_gas_station", text: "Combustível" },
+  { icon: "credit_card", text: "Cartão de Crédito" },
+  { icon: "local_hospital", text: "Saúde" },
+  { icon: "receipt_long", text: "Contas" },
+  { icon: "electric_bolt", text: "Energia" },
+  { icon: "local_grocery_store", text: "Supermercado" },
+  { icon: "pets", text: "Pets" },
+  { icon: "school", text: "Educação" },
+  { icon: "directions_bus", text: "Transporte" },
+  { icon: "house", text: "Moradia" },
+  { icon: "phone_iphone", text: "Telefonia" },
+  { icon: "wifi", text: "Internet" },
+  { icon: "tv", text: "TV/Streaming" },
+  { icon: "fitness_center", text: "Academia" },
+  { icon: "local_bar", text: "Lazer" },
+  { icon: "sports_esports", text: "Jogos" },
+  { icon: "beach_access", text: "Viagem" },
+  { icon: "theaters", text: "Cinema" },
+  { icon: "park", text: "Parque" },
+  { icon: "liquor", text: "Bebidas" },
+  { icon: "shopping_bag", text: "Roupas" },
+  { icon: "child_friendly", text: "Crianças" },
+  { icon: "medical_services", text: "Farmácia" },
+  { icon: "payments", text: "Impostos" },
+  { icon: "construction", text: "Reparos" },
+  { icon: "devices_other", text: "Eletrônicos" }
+
+];
+/*--28X-*/
+
+function test_display() {
+  test_new_categorie = document.getElementById('form_new_categorie');
+  test_display_on = document.getElementById('display_icon_on');
+  test_contem = test_new_categorie.contains(test_display_on)
+  console.log(test_contem)
+
+  if (!test_contem) {
+    criate_icons()
+  } else if (test_contem) {
+    test_display_on.remove()
+
+  }
+}
+
+
+
+
+function criate_icons() {
+  form_new_categorie = document.getElementById('form_new_categorie');
+
+  const display_icon_on = document.createElement('div');
+  display_icon_on.id = 'display_icon_on'
+  form_new_categorie.appendChild(display_icon_on);
+
+  bank_icons.forEach(function (item, index) {
+    const new_button = document.createElement('button');
+    const new_icons = document.createElement('span');
+
+    new_button.className = "list_button_icons";
+    new_icons.className = "material-symbols-outlined", "new_icon";
+    new_icons.textContent = item.icon;
+
+    new_button.appendChild(new_icons);
+
+    //  icons_list = document.getElementById('icons_list');
+
+    //  icons_list.appendChild(new_icons)
+    display_icon_on.appendChild(new_button);
+
+  })
+}
 /*-------COM O ONLCLICK-----------
 function verificaRadio() {
   const question_repet = document.getElementById("question_repet");
