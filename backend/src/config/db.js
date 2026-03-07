9f6cc394649875cceddbe1e6d2ab670f3aeb6acb
donizeti26 <silva.donizeti26@gmail.com>
2026-03-07 11:16:31 -0300

    testando se coneção com vercel  e neon funcionaram

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,12 +3,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 // conexão com o PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 console.log(process.env.DB_USER + " As");
