7b45e263d3d4b7cfc01de522a959f81be67af756
donizeti26 <silva.donizeti26@gmail.com>
2026-03-10 12:17:42 -0300

    rota teste banco

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-import pool from "./db.js";
+import pool from "./config/db.js";
 // Importando rotas
 
 import routes from "./routes/index.js";
