9bbb5e6fb4707e49075d0cc69033b971844a1c10
donizeti26 <silva.donizeti26@gmail.com>
2026-03-10 22:55:25 -0300

    alterando fetch

--- frontend/src/script/api.js
+++ frontend/src/script/api.js
@@ -1,10 +1,13 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function apiFetch(endpoint, options = {}) {
-  const response = await fetch(`${process.env.production.API_URL}${endpoint}`, {
+  const response = await fetch(`${API_URL}${endpoint}`, {
     headers: {
       "Content-Type": "application/json",
     },
     ...options,
   });
+
   if (!response.ok) {
     throw new Error("Erro na requisição");
   }
